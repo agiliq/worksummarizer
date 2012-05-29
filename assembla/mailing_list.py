8968802b255cad69ea8468668737782c3689acfd
arjunc77 <arjunchitturi@gmail.com>
2012-05-29 20:34:57 +0530

    mv mailin list file here

---
+++ assembla/mailing_list.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 *-*
+# -*- coding: utf-8 *-*
+email_to = [
+    ('ramana@agiliq.com',  # Email Id
+    'Venkata Ramana C'  # Name
+    ),
+    ('dheeru@agiliq.com', 'Dhreej Kumar'),
+    ]
