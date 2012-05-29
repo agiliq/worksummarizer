74f02b94f352bd84656e78c3fde6f0bdecce15a6
arjunc77 <arjunchitturi@gmail.com>
2012-05-29 20:41:12 +0530

    rm mailin list file frm core folder

--- assembla/assembla/mailing_list.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 *-*
-# -*- coding: utf-8 *-*
-email_to = [
-    ('ramana@agiliq.com',  # Email Id
-    'Venkata Ramana C'  # Name
-    ),
-    ('dheeru@agiliq.com', 'Dhreej Kumar'),
-    ]
