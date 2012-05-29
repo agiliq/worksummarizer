8182b8cacebc1ef480e39445e3f3c1cb88c0668c
arjunc77 <arjunchitturi@gmail.com>
2012-05-29 20:40:25 +0530

    mv mailin list file here

--- assembla/mailing_list.py
+++ assembla/mailing_list.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 *-*
-# -*- coding: utf-8 *-*
+#add people to whom summary mail should be sent.
 email_to = [
     ('ramana@agiliq.com',  # Email Id
     'Venkata Ramana C'  # Name
